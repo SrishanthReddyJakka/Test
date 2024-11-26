047746c8d712e274d3fdff39e7317dc629579592
SrishanthReddyJakka <jakkasrishanthreddy@gmail.com>
2024-11-26 10:59:29 +0000

    conversion

---
+++ basics/3_conversionOperation.js
@@ -0,0 +1,51 @@
+let score ="33abc"
+console.log(typeof score);
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//------------------------------
+let score1 =null
+console.log(typeof score1);
+
+let valueInNumber1=Number(score1);
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1)
+
+//--------------------------------
+let score2 =undefined
+console.log(typeof score2);
+
+let valueInNumber2=Number(score2);
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2)
+
+//--------------------------------
+
+/* 
+"33"->33
+"33abc"->Nan
+null-> 0
+undefined -> Nan
+*/
+
+let isLog=1;
+console.log(isLog)
+let b=Boolean(isLog)
+console.log(b)
+
+
+let isLog1="";
+console.log(isLog1)
+let b1=Boolean(isLog1)
+console.log(b1)
+
+
+let isLog2="ram";
+console.log(isLog2)
+let b2=Boolean(isLog2)
+console.log(b2)
+
+
+
